8cd4d4afc85aedb4a9fd2ce5091523d6ac3c05ac
merasziaddocchim <ziadmeras92@gmail.com>
2025-05-13 10:42:36 +0100

    Update chat.js

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -1,10 +1,21 @@
+import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
 import ChatInterface from '@/components/ChatInterface'
+import { useEffect, useState } from 'react'
 
 export default function ChatPage() {
+  const router = useRouter()
+  const [initialQuery, setInitialQuery] = useState('')
+
+  useEffect(() => {
+    if (router.query.question) {
+      setInitialQuery(router.query.question)
+    }
+  }, [router.query.question])
+
   return (
     <Layout>
-      <ChatInterface />
+      <ChatInterface initialQuery={initialQuery} />
     </Layout>
   )
 }
