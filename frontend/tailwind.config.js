ef7fefd7daa147f50779da46450ab2d4b3486ce2
merasziaddocchim <ziadmeras92@gmail.com>
2025-05-13 22:47:12 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {require('@tailwindcss/typography')},
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), // this is now in the correct place
+  ],
 }
