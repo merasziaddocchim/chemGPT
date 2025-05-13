fffc79a1751d4af0934a2f30fc3328ab7981bc73
merasziaddocchim <ziadmeras92@gmail.com>
2025-05-13 23:50:37 +0100

    Update MessageList.js

--- frontend/components/MessageList.js
+++ frontend/components/MessageList.js
@@ -1,12 +1,11 @@
-// frontend/components/MessageList.js
-import Message from './Message';
+import Message from './Message'
 
 export default function MessageList({ messages }) {
   return (
-    <div className="flex-1 overflow-y-auto p-4">
-      {messages.map((msg, index) => (
-        <Message key={index} role={msg.role} content={msg.content} />
+    <div className="flex-1 px-6 py-4 overflow-y-auto">
+      {messages.map((msg, i) => (
+        <Message key={i} content={msg.content} role={msg.role} />
       ))}
     </div>
-  );
+  )
 }
