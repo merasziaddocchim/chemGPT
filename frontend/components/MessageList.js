3645aed56b2156aa5ddfc614383def42baf9adc2
merasziaddocchim <ziadmeras92@gmail.com>
2025-05-14 18:22:37 +0100

    Update MessageList.js

--- frontend/components/MessageList.js
+++ frontend/components/MessageList.js
@@ -1,6 +1,8 @@
 import Message from './Message'
 
 export default function MessageList({ messages }) {
+  console.log("MessageList received:", messages);
+
   return (
     <div className="flex-1 px-6 py-4 overflow-y-auto">
       {messages.map((msg, i) => (
