9c90645b02a9e91b3279d21440f6407ee728ec69
merasziaddocchim <ziadmeras92@gmail.com>
2025-05-15 18:36:03 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Link from 'next/link';
 
 export default function HomePage() {
   const [query, setQuery] = useState("");
